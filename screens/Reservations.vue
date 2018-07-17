91381d3ad83a6b544a7b1d82031f46e10a83ef7b
Travis Roach <travis.roach24@gmail.com>
2018-07-17 14:37:58 -0600

    add footer to booking and ordering screens

--- screens/Reservations.vue
+++ screens/Reservations.vue
@@ -17,5 +17,78 @@
       </nb-body>
       <nb-right />
     </nb-header>
+    <nb-content></nb-content>
+    <nb-footer>
+      <nb-footer-tab>
+        <nb-button :active="tab1" :onPress="toggleTab1">
+          <nb-icon name="flag" :active="tab1" />
+          <nb-text>Book</nb-text>
+        </nb-button>
+        <nb-button :active="tab2" :onPress="toggleTab2">
+            <nb-icon name="pizza" :active="tab2" />
+            <nb-text>Eat</nb-text>
+        </nb-button>
+        <nb-button :active="tab3" :onPress="toggleTab3">
+            <nb-icon name="beer" :active="tab3" />
+            <nb-text>Drink</nb-text>
+        </nb-button>
+        <nb-button :active="tab4" :onPress="toggleTab4">
+            <nb-icon name="contact" :active="tab4" />
+            <nb-text>Account</nb-text>
+        </nb-button>
+      </nb-footer-tab>
+    </nb-footer>
   </nb-container>
 </template>
+
+<script>
+  export default {
+  props: {
+    navigation: {
+      type: Object
+    },
+  },
+  data: function() {
+    return {
+      tab1: false,
+      tab2: false,
+      tab3: false,
+      tab4: false
+    };
+  },
+  methods: {
+    openReservations() {
+      this.navigation.navigate("Reservations");
+    },
+    openFoodDrink() {
+      this.navigation.navigate("FoodDrink");
+    },
+    toggleTab1: function() {
+      this.tab1 = true;
+      this.navigation.navigate('Reservations');
+      this.tab2 = false;
+      this.tab3 = false;
+      this.tab4 = false;
+    },
+    toggleTab2: function() {
+      this.tab1 = false;
+      this.tab2 = true;
+      this.navigation.navigate("FoodDrink");
+      this.tab3 = false;
+      this.tab4 = false;
+    },
+    toggleTab3: function() {
+      this.tab1 = false;
+      this.tab2 = false;
+      this.tab3 = true;
+      this.tab4 = false;
+    },
+    toggleTab4: function() {
+      this.tab1 = false;
+      this.tab2 = false;
+      this.tab3 = false;
+      this.tab4 = true;
+    }
+  },
+};
+</script>
